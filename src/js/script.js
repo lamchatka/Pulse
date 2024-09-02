96a5d4a49a582b307d9215ab65724d3e68433a1f
Maxim Rukoleev <maxprolife@icloud.com>
2024-09-02 13:16:34 +0300

    added scripts for tabs and toggle slides

--- src/js/script.js
+++ src/js/script.js
@@ -13,5 +13,25 @@ $(document).ready(function(){
                 }
             },
         ]
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active')
+
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-card__content').eq(i).toggleClass('catalog-card__content_active');
+                $('.catalog-card__list').eq(i).toggleClass('catalog-card__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-card__link');
+    toggleSlide('.catalog-card__back');
 });
-})
